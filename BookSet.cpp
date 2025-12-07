6ad7615cfdf29ab3856b1817f3875a75c4565f6b
Patrick Donaghy <235614726+Varreno@users.noreply.github.com>
2025-12-07 10:56:01 -0800

    Changed to std::cout for consistentcy
    Updated printDetails method to use std::cout for consistency.

--- BookSet.cpp
+++ BookSet.cpp
@@ -1,6 +1,6 @@
 #include "BookSet.h"
 #include <iostream>
-using namespace std;
+
 
 // TODO: Implement the Constructor
 // CRITICAL HINT: You must call the BOOK constructor in your initialization list.
@@ -12,9 +12,9 @@ void BookSet::printDetails() {
     // TODO: Print the details. 
     // You can print "Type: BOOK SET (Vol " << volume << ")"
     // You can manually print the title/author, OR you can call Book::printDetails() inside here.
-    cout << "Type: BOOK SET (Vol " << volume << ")" << endl;
-    cout << "Title: " << title << "Price: $" << price << endl;
-    cout << "Author: " << author << endl << "ISBN: " << isbn << endl;
-    cout << "Volume: " << volume << endl;
+    std::cout << "Type: BOOK SET (Vol " << volume << ")" << std::endl;
+    std::cout << "Title: " << title << "Price: $" << price << std::endl;
+    std::cout << "Author: " << author << std::endl << "ISBN: " << isbn << std::endl;
+    std::cout << "Volume: " << volume << std::endl;
     std::cout << "-----------------------------" << std::endl;
 }
